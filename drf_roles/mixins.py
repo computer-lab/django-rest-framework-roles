32e32c8495d9d949dea53c002212e9b8bf7105b5
Robert C Jensen <robert.cole.jensen@gmail.com>
2016-08-23 22:03:11 -0400

    gracefully degrade when user invariants break

--- drf_roles/mixins.py
+++ drf_roles/mixins.py
@@ -11,6 +11,10 @@ DEFAULT_REGISTRY = (
     "perform_destroy",
 )
 
+class RoleError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
 
 class RoleViewSetMixin(object):
     """A ViewSet mixin that parameterizes DRF methods over roles"""
@@ -19,25 +23,22 @@ class RoleViewSetMixin(object):
 
     def _call_role_fn(self, fn, *args, **kwargs):
         """Attempts to call a role-scoped method"""
-
-        role_name = self._get_role(self.request.user)
-        role_fn = "{}_for_{}".format(fn, role_name)
-
         try:
+            role_name = self._get_role(self.request.user)
+            role_fn = "{}_for_{}".format(fn, role_name)
             return getattr(self, role_fn)(*args, **kwargs)
-        except AttributeError:
+        except (AttributeError, RoleError):
             return getattr(super(RoleViewSetMixin, self), fn)(*args, **kwargs)
 
     def _get_role(self, user):
         """Retrieves the given user's role"""
-
         user_groups = set([group.name.lower() for group in user.groups.all()])
         user_role = self._role_groups.intersection(user_groups)
 
         if len(user_role) < 1:
-            raise Exception("The user is not a member of any role groups")
+            raise RoleError("The user is not a member of any role groups")
         elif len(user_role) > 1:
-            raise Exception("The user is a member of multiple role groups")
+            raise RoleError("The user is a member of multiple role groups")
         else:
             return user_role.pop()
 
